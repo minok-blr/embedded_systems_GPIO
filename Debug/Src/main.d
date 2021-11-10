0e0d5e1acded71de6a940dcaca12a4c96ec8915f
Luka Jeraminok <luka.jeraminok@student.um.si>
2021-11-10 12:53:44 +0100

    Final solution

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c ../Inc/memorymap.h
+
+../Inc/memorymap.h:
---
+++ Inc/memorymap.h
@@ -0,0 +1,34 @@
+/*
+ * memorymap.h
+ *
+ *  Created on: Oct 20, 2021
+ *      Author: luka.jeraminok
+ */
+
+#ifndef MEMORYMAP_H_
+#define MEMORYMAP_H_
+
+#define RCC_BASE 0x40023800
+#define RCC_AHB1ENR (RCC_BASE + 0x30)
+#define RCC_AHB1ENR_GPIOGEN (1<<6)
+#define RCC_AHB1ENR_GPIOAEN (1<<0)
+
+#define GPIOA_BASE 0x40020000
+#define GPIOA_MODER (GPIOA_BASE + 0x00)
+#define GPIOA_IDR (GPIOA_BASE + 0x10)
+
+#define GPIOG_BASE 0x40021800
+#define GPIOG_MODER (GPIOG_BASE + 0X00)
+#define GPIOG_ODR (GPIOG_BASE + 0x14)
+
+#define RED_LED_PIN (1 << 14)
+#define USER_BUTTON_PIN (1 << 0)
+
+// task 2
+#define RCC_AHB1ENR_GPIOFEN (1<<5)
+#define GPIOF_BASE 0x40021400
+#define GPIOF_MODER (GPIOF_BASE + 0x00)
+#define GPIOF_ODR (GPIOF_BASE + 0x14)
+
+
+#endif /* MEMORYMAP_H_ */
